c99b3d3d3ff957924ee4b74c5bb071803068e5d1
Furkan GÜLER <44furkanguler07@gmail.com>
2023-01-31 04:18:07 +0300

    com

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { RatingModule } from 'ng-starrating';
 import { SearchComponent } from './search/search.component';
@@ -16,7 +15,6 @@ import { CartPageComponent } from './cart-page/cart-page.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
     HomeComponent,
     SearchComponent,
     FurnPageComponent,
